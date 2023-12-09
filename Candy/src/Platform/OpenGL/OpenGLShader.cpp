a0c69a0899ab8f45d7e2fda98e6b61532e421057
tangxiaolong <xltangcs@outlook.com>
2023-12-10 00:22:53 +0800

    add rotation

--- Candy/src/Platform/OpenGL/OpenGLShader.cpp
+++ Candy/src/Platform/OpenGL/OpenGLShader.cpp
@@ -191,6 +191,10 @@ namespace Candy {
 	{
 		UploadUniformInt(name, value);
 	}
+	void OpenGLShader::SetFloat(const std::string& name, float value)
+	{
+		UploadUniformFloat(name, value);
+	}
 
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -29,9 +29,12 @@ void Sandbox2D::OnUpdate(Candy::Timestep ts)
 	Candy::RenderCommand::Clear();
 
 	Candy::Renderer2D::BeginScene(m_CameraController.GetCamera());
+
 	Candy::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	Candy::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	Candy::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+	Candy::Renderer2D::DrawRotatedQuad({ 0.25f, 0.25f }, { 1.0f, 1.0f }, 45, { 0.8f, 0.2f, 0.2f, 1.0f });
+
 	Candy::Renderer2D::EndScene();
 }
 
