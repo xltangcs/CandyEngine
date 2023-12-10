d7b7ed5c23f6c9d457ad36d7e513e12921c9ce7b
tangxiaolong <xltangcs@outlook.com>
2023-12-11 00:47:03 +0800

    add batch render

--- Candy/src/Candy/Renderer/Buffer.cpp
+++ Candy/src/Candy/Renderer/Buffer.cpp
@@ -6,6 +6,19 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Candy {
+
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    CANDY_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		CANDY_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
