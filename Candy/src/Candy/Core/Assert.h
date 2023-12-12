e42c041f496ffd340ed7de507a8fe4658366dca0
tangxiaolong <xltangcs@outlook.com>
2023-12-12 16:15:13 +0800

    Added Sprite Renderer Component UI

--- Candy/src/Candy/Core/Assert.h
+++ Candy/src/Candy/Core/Assert.h
@@ -1,31 +1,17 @@
 #pragma once
 
+#include <filesystem>
+
 #include "Candy/Core/Base.h"
 #include "Candy/Core/Log.h"
 
 #ifdef CANDY_ENABLE_ASSERTS
 
-namespace Candy::Assert
-{
-	// Returns the simple file name rather than full path as suggested by LovelySanta
-	constexpr const char* CurrentFileName(const char* path) {
-		const char* file = path;
-		while (*path)
-		{
-			if (*path == '/' || *path == '\\')
-				file = ++path;
-			else
-				path++;
-		}
-		return file;
-	}
-}
-
 // Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 // provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 #define CANDY_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { CANDY##type##ERROR(msg, __VA_ARGS__); CANDY_DEBUGBREAK(); } }
 #define CANDY_INTERNAL_ASSERT_WITH_MSG(type, check, ...) CANDY_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-#define CANDY_INTERNAL_ASSERT_NO_MSG(type, check) CANDY_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", CANDY_STRINGIFY_MACRO(check), ::Candy::Assert::CurrentFileName(__FILE__), __LINE__)
+#define CANDY_INTERNAL_ASSERT_NO_MSG(type, check) CANDY_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", CANDY_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 #define CANDY_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 #define CANDY_INTERNAL_ASSERT_GET_MACRO(...) CANDY_EXPAND_MACRO( CANDY_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, CANDY_INTERNAL_ASSERT_WITH_MSG, CANDY_INTERNAL_ASSERT_NO_MSG) )
