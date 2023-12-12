69114fd2c4cb540aae887a5a4729e0e32c7435fd
tangxiaolong <xltangcs@outlook.com>
2023-12-12 17:40:56 +0800

    Now can add or delete entities and components

--- Candy/src/Candy/Scene/Scene.cpp
+++ Candy/src/Candy/Scene/Scene.cpp
@@ -29,6 +29,11 @@ namespace Candy {
 		return entity;
 	}
 
+	void Scene::DestroyEntity(Entity entity)
+	{
+		m_Registry.destroy(entity);
+	}
+
 	void Scene::OnUpdate(Timestep ts)
 	{
 		// Update scripts
@@ -93,5 +98,35 @@ namespace Candy {
 				cameraComponent.Camera.SetViewportSize(width, height);
 		}
 	}
+	template<typename T>
+	void Scene::OnComponentAdded(Entity entity, T& component)
+	{
+		static_assert(false);
+	}
 
+	template<>
+	void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component)
+	{
+		component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<NativeScriptComponent>(Entity entity, NativeScriptComponent& component)
+	{
+	}
 }
