9246ef002439a64af8f9387c2b27d5fe93b5127e
tangxiaolong <xltangcs@outlook.com>
2023-12-12 16:39:36 +0800

    Transform component now can support modify translation, rotation and scale

--- Candy/src/Candy/Renderer/Buffer.h
+++ Candy/src/Candy/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace Candy {
 	public:
 		BufferLayout() = default;
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
